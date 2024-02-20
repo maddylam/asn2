9d6bda574d3d1ce3e78a87054e7772e3db527900
maddylam <maddylam04@gmail.com>
2024-02-19 23:40:47 -0800

    created a delete student option

--- src/main/java/com/cmpt276/asn2/controllers/StudentsController.java
+++ src/main/java/com/cmpt276/asn2/controllers/StudentsController.java
@@ -1,8 +1,8 @@
 package com.cmpt276.asn2.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -39,11 +39,37 @@ public class StudentsController {
         int newHeight = Integer.parseInt(newStudent.get("height"));
         String newHairColour = newStudent.get("hairColour");
         double newGpa = Double.parseDouble(newStudent.get("gpa"));
-        //int newGpa = Integer.parseInt(newStudent.get("gpa"));
         String newFavColour = newStudent.get("favColour");
         studentRepo.save(new Student(newName, newWeight, newHeight, newHairColour, newGpa, newFavColour)); 
         response.setStatus(201);
 
         return "students/addedStudent"; 
     }
+
+    @PostMapping("/students/delete")
+    public String deleteStudent(@RequestParam("uid") int uid, HttpServletResponse response){
+        System.out.println("DELETE student");
+
+        //get user id 
+        Optional<Student> toDeleteSt = studentRepo.findById(uid);
+
+        //check if user id is valid, if not then show user an error 
+        if(toDeleteSt.isPresent()){
+            //delete the student 
+            studentRepo.delete(toDeleteSt.get());
+            response.setStatus(200);
+
+        } else {
+            //show error 
+            response.setStatus(404);
+        }
+        return "students/addedStudent";
+    }
+
+    //postmapping for updating student 
+    //ask for the student's uid 
+    //if the uid exists: 
+        //take in all values again for the new student attributes
+    //if it doesnt exist, show error
+    
 }
--- src/main/java/com/cmpt276/asn2/models/StudentRepository.java
+++ src/main/java/com/cmpt276/asn2/models/StudentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StudentRepository extends JpaRepository<Student,Integer>{
     List<Student> findByName(String name);
     List<Student> findByWeight(int weight);
+    List<Student> findByUid(int uid);
 }
